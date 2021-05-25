2fe3017249f643ff1470d6f755c4f3a55cc4c70e
Franz Dietrich <dietrich@teilgedanken.de>
2021-05-25 20:13:56 +0200

    restructuring move translations to toplevel

--- app/src/i18n.rs
+++ app/src/i18n.rs
@@ -81,7 +81,7 @@ impl Lang {
     pub const fn ftl_messages(self) -> &'static str {
         macro_rules! include_ftl_messages {
             ( $lang_id:literal ) => {
-                include_str!(concat!("../locales/", $lang_id, "/main.ftl"))
+                include_str!(concat!("../../locales/", $lang_id, "/main.ftl"))
             };
         }
         match self {
